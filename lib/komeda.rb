a91c1d82dbad6a8d1bc807c9eeeb3de559fd9f26
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2023-11-12 13:13:42 +0900

    Implemented main functionality

--- lib/komeda.rb
+++ lib/komeda.rb
@@ -1,8 +1,29 @@
 # frozen_string_literal: true
 
+require 'yaml'
+require 'hashie'
+require 'active_support/all'
+require_relative 'komeda/item'
 require_relative 'komeda/version'
 
 module Komeda
-  class Error < StandardError; end
-  # Your code goes here...
+  class << self
+    def method_missing(name, *args, &block)
+      if (menu = menus[name])
+        menu.map { |item| Komeda::Item[item.except(:id, :updated_at)] }
+      else
+        super(name, *args, &block)
+      end
+    end
+
+    def respond_to_missing?(name, include_private)
+      menus[name] ? true : super(name, include_private)
+    end
+
+    private
+
+    def menus
+      @menus ||= YAML.load_file('./config/menus.yaml', aliases: true, permitted_classes: [Time]).deep_symbolize_keys
+    end
+  end
 end
--- spec/komeda_spec.rb
+++ spec/komeda_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Komeda do
   it 'has a version number' do
     expect(Komeda::VERSION).not_to be nil
   end
+
+  it 'can fetch all menu' do
+    expect(Komeda.all).not_to be nil
+  end
 end
